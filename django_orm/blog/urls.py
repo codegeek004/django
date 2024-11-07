2220280cd5b835a7fc458519ca521dff67250ca8
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-08 01:30:53 +0530

    Final commit

--- django_orm/blog/urls.py
+++ django_orm/blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	path('record/<int:pk>', views.customer_record, name='record'),
 	path('record/delete/<int:pk>', views.delete_record, name='delete_record'),
 	path('record/add', views.add_record, name='add_record'),
+	path('record/update/<int:pk>', views.update_record,name='update_record'),
 
 ]
