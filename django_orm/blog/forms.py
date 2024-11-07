dce81c232a09429ccba1921af647233d26195998
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-08 01:07:13 +0530

    Add record functionality added

--- django_orm/blog/forms.py
+++ django_orm/blog/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
 	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
@@ -27,3 +28,21 @@ class SignUpForm(UserCreationForm):
 		self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
 		self.fields['password2'].label = ''
 		self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
+
+#form for adding record
+class AddRecordForm(forms.ModelForm):
+	first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="") 
+	last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="") 
+	email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"email", "class":"form-control"}), label="") 
+	phone =  forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"phone", "class":"form-control"}), label="") 
+	address =  forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"address", "class":"form-control"}), label="") 
+	city =  forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"city", "class":"form-control"}), label="") 
+	state =  forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"state", "class":"form-control"}), label="") 
+	pincode =  forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"pincode", "class":"form-control"}), label="") 
+
+
+	class Meta:
+		model = Record 
+		exclude = ("user",)
--- django_orm/blog/urls.py
+++ django_orm/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path('register/', views.register_user, name='register'),
 	path('logout/', views.logout_user, name='logout'),
 	path('record/<int:pk>', views.customer_record, name='record'),
-	path('record/delete/<int:pk>', views.delete_record, name='delete_record')
+	path('record/delete/<int:pk>', views.delete_record, name='delete_record'),
+	path('record/add', views.add_record, name='add_record'),
 
 ]
