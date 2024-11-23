a70d7e628146533a30c0b84288f66c6b4c549c0f
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-23 22:04:28 +0530

    models

--- django_database/djangodb/website/models.py
+++ django_database/djangodb/website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Members(models.Model):
+	fname = models.CharField(max_length=50)
+	lname = models.CharField(max_length=50)
+	email = models.EmailField(max_length=150)
+	password = models.CharField(max_length=200)
+	age = models.IntegerField()
+	address = models.CharField(max_length=100)
+	
+	#what we display on admin as members
+	def __str__(self):
+		return self.fname + ' ' + self.lname
