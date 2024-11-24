59492a5fe776d4b0762cc78f911741f74579106f
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-24 16:30:57 +0530

    Multiple inheritence

--- learning_database/demo_database/myapp/models.py
+++ learning_database/demo_database/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#####for multiple inheritence#####
 class Quiz(models.Model):
 	name = models.CharField(max_length=256)
 
@@ -15,7 +16,6 @@ class Question(models.Model):
 	order = models.SmallIntegerField()
 	class Meta:
 		ordering = ('order',)
-		abstract=True
 	def __str__(self):
 		return self.text
 
@@ -25,3 +25,32 @@ class MCQuestion(Question):
 
 class BooleanQuestion(Question):
 	is_true = models.BooleanField()
+
+
+######for abstract inheritence########
+# class Quiz(models.Model):
+# 	name = models.CharField(max_length=256)
+
+# 	def __str__(self):
+# 		return self.name
+
+# class Question(models.Model):
+# 	quiz = models.ForeignKey(
+# 		Quiz,
+# 		on_delete=models.CASCADE,
+# 		related_name='%(class)s'
+# 		)
+# 	text = models.CharField(max_length=1000)
+# 	order = models.SmallIntegerField()
+# 	class Meta:
+# 		ordering = ('order',)
+# 		abstract=True
+# 	def __str__(self):
+# 		return self.text
+
+
+# class MCQuestion(Question):
+# 	number_of_answers = models.PositiveSmallIntegerField()
+
+# class BooleanQuestion(Question):
+# 	is_true = models.BooleanField()
