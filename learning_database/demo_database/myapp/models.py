ae965041408049dc04d01b7b59c0672ab84c2c37
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-24 12:37:33 +0530

    learning_orm

---
+++ learning_database/demo_database/myapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+class Quiz(models.Model):
+	name = models.CharField(max_length=256)
+
+	def __str__(self):
+		return self.name
+
+class Question(models.Model):
+	quiz = models.ForeignKey(
+		Quiz,
+		on_delete=models.CASCADE,
+		related_name='%(class)s'
+		)
+	text = models.CharField(max_length=1000)
+	order = models.SmallIntegerField()
+	class Meta:
+		ordering = ('order',)
+		abstract=True
+	def __str__(self):
+		return self.text
+
+
+class MCQuestion(Question):
+	number_of_answers = models.PositiveSmallIntegerField()
+
+class BooleanQuestion(Question):
+	is_true = models.BooleanField()
